1071ade83a74865a29d4ec13b10e71fac8315abc
Manfred Jantscher <manfred@Manfreds-MacBook-Pro.local>
2016-07-16 15:03:47 +0200

    Fix bug

--- Arygon/module.php
+++ Arygon/module.php
@@ -19,11 +19,6 @@
 
             //Connect Parent
             $this->RequireParent("{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}");
-            $pid = $this->GetParent();
-            if ($pid) {
-                $name = IPS_GetName($pid);
-                if ($name == "Serial Port") IPS_SetName($pid, __CLASS__ . " Port");
-            }
 
         }
  
@@ -39,6 +34,41 @@
         public function ApplyChanges() {
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
+
+        //prüfen ob IO ein SerialPort ist
+        //        
+        // Zwangskonfiguration des SerialPort, wenn vorhanden und verbunden
+        // Aber nie bei einem Neustart :)
+        if (IPS_GetKernelRunlevel() == KR_READY)
+        {
+            $ParentID = $this->GetParent();
+            if (!($ParentID === false))
+            {
+                $ParentInstance = IPS_GetInstance($ParentID);
+                if ($ParentInstance['ModuleInfo']['ModuleID'] == '{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}')
+                {
+                    if (IPS_GetProperty($ParentID, 'StopBits') <> '1')
+                        IPS_SetProperty($ParentID, 'StopBits', '1');
+                    if (IPS_GetProperty($ParentID, 'BaudRate') <> '9600')
+                        IPS_SetProperty($ParentID, 'BaudRate', '9600');
+                    if (IPS_GetProperty($ParentID, 'Parity') <> 'None')
+                        IPS_SetProperty($ParentID, 'Parity', 'None');
+                    if (IPS_GetProperty($ParentID, 'DataBits') <> '8')
+                        IPS_SetProperty($ParentID, 'DataBits', '8');
+                    if (IPS_HasChanges($ParentID))
+                        IPS_ApplyChanges($ParentID);
+                }
+            }
+        }
+        try
+        {
+            $this->SenCommand("0asn");
+        } catch (Exception $exc)
+        {
+            if (IPS_GetKernelRunlevel() == KR_READY)
+                trigger_error($exc->getMessage(), $exc->getCode());
+        }
+
         }
  
         // Ich empfange die Daten vom Parent im String $JSONString
@@ -74,4 +104,12 @@
             return $this->SendCommand($cmd);
         }
     }
+
+
+    protected function GetParent()
+    {
+        $instance = @IPS_GetInstance($this->InstanceID);
+        return ($instance['ConnectionID'] > 0) ? $instance['ConnectionID'] : false;
+    }
+
 ?>
