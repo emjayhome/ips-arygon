3df90a66523c9b56cf25c8e9545450a7fe25acad
Manfred Jantscher <manfred@Manfreds-MacBook-Pro.local>
2016-07-24 16:10:13 +0200

    Bugfix.

--- ArygonSplitter/module.php
+++ ArygonSplitter/module.php
@@ -9,6 +9,7 @@ class ArygonSplitter extends IPSModule {
     public function Create() {
         parent::Create();
         $this->RequireParent("{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}");
+        IPS_LogMessage('ArygonSplitter', 'Create');
     }
 
     public function ApplyChanges() {
