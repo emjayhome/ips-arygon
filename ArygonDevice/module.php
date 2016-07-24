90a35fd9f5ac5fbbae58beaeaf81b1e233647a9c
Manfred Jantscher <manfred@Manfreds-MacBook-Pro.local>
2016-07-24 14:04:52 +0200

    New structure.

---
+++ ArygonDevice/module.php
@@ -0,0 +1,210 @@
+<?
+
+require_once(__DIR__ . "/../ArygonData.php");
+require_once(__DIR__ . "/../IpsIncludes.php");
+
+class ArygonDevice extends IPSModule {
+
+    // IPS module functions
+
+    public function Create() {
+        parent::Create();
+        $this->ConnectParent("{2F75DC6B-F9AA-4A2E-B121-F76FBC365566}");
+    }
+
+    public function ApplyChanges() {
+        parent::ApplyChanges();
+        $this->RegisterVariableString("ResponseData", "ResponseData", "", -1);
+        IPS_SetHidden($this->GetIDForIdent('ResponseData'), true);
+        $this->RegisterVariableString("CardUID", "CardUID", "", 0);
+        $this->RegisterVariableString("ReaderState", "ReaderState", "", 1);
+  
+        try {
+            $this->ResetReader();
+        } catch (Exception $ex) {
+            unset($ex);
+        }
+    }
+
+    protected function HasActiveParent() {         
+        $instance = IPS_GetInstance($this->InstanceID);
+        if ($instance['ConnectionID'] > 0) {
+            $parent = IPS_GetInstance($instance['ConnectionID']);
+            if ($parent['InstanceStatus'] == IS_ACTIVE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected function GetVariable($Ident, $VarType, $VarName, $Profile, $EnableAction)
+    {
+        $VarID = @$this->GetIDForIdent($Ident);
+        if ($VarID > 0)
+        {
+            if (IPS_GetVariable($VarID)['VariableType'] <> $VarType)
+            {
+                IPS_DeleteVariable($VarID);
+                $VarID = false;
+            }
+        }
+        if ($VarID === false)
+        {
+            $this->MaintainVariable($Ident, $VarName, $VarType, $Profile, 0, true);
+            if ($EnableAction)
+                $this->MaintainAction($Ident, true);
+            $VarID = $this->GetIDForIdent($Ident);
+        }
+        return $VarID;
+    }
+
+    protected function SetStatus($InstanceStatus) {
+        if ($InstanceStatus <> IPS_GetInstance($this->InstanceID)['InstanceStatus']) {
+            parent::SetStatus($InstanceStatus);
+        }
+    }
+
+    // IPS raw data iterface for parent (splitter)
+    public function ReceiveData($JSONString) {
+        $ResponseData = json_decode($JSONString);
+
+        if ($ResponseData->DataID <> '{35B444C9-CDC0-4F0F-BEBD-A5BDD29D07A4}') {
+            return false;
+        }
+
+        $Response = new ArygonResponseASCII();
+        $Response->GetDataFromJSONObject($ResponseData);
+
+        if (!$this->lock('ResponseData')) {
+            throw new Exception('ResponseData is locked', E_USER_NOTICE);
+        }
+        SetValueString($ResponseDataID, $Response->GetRawResponse());
+        $this->unlock('ResponseData');
+    }
+
+    // Reinitialize reader
+    public function ResetReader() {
+
+        // Initiate uC software reset (TAMA is reset as well)
+        $Command = new ArygonCommandASCII();
+        $Command->SetCommand('au');
+        try {
+            $this->Send($Command, false);
+        } catch (Exception $exc) {
+                unset($exc);
+        }
+
+        // Wait
+        IPS_Sleep(100);
+
+        // Get uC firmware version
+        $Command = new ArygonCommandASCII();
+        $Command->SetCommand('av');
+        try {
+            $result = $this->Send($Command, true);
+        } catch (Exception $exc) {
+                unset($exc);
+        } 
+
+        // Get the unique serial number of the reader
+        $Command = new ArygonCommandASCII();
+        $Command->SetCommand('asn');
+        try {
+            $result = $this->Send($Command, true);
+        } catch (Exception $exc) {
+                unset($exc);
+        }           
+
+    }
+
+    private function Send(ArygonCommandASCII $Command, $needResponse = true) {
+        if (!$this->HasActiveParent()) {
+            throw new Exception("Instance has no active Parent.", E_USER_NOTICE);
+        }
+
+        $ResponseDataID = $this->GetIDForIdent('ResponseData');
+        if (!$this->lock('RequestSendData')) {
+            throw new Exception('RequestSendData is locked', E_USER_NOTICE);
+        }
+
+        if ($needResponse) {
+            if (!$this->lock('ResponseData')) {
+                $this->unlock('RequestSendData');
+                throw new Exception('ResponseData is locked', E_USER_NOTICE);
+            }
+            SetValueString($ResponseDataID, '');
+            $this->unlock('ResponseData');
+        }
+
+        $ret = $this->SendDataToParent($Command);
+        if ($ret === false) {
+            $this->unlock('RequestSendData');
+            throw new Exception('Instance has no active Parent Instance!', E_USER_NOTICE);
+        }
+
+        if (!$needResponse) {
+            $this->unlock('RequestSendData');
+            return true;
+        }
+        $Response = $this->WaitForResponse();
+
+        if ($Response === false) {
+            $this->unlock('RequestSendData');
+            throw new Exception('Send Data Timeout', E_USER_NOTICE);
+        }
+
+        $this->unlock('RequestSendData');
+        return $Response;
+    }
+
+    protected function SendDataToParent($Data) {
+        $JSONString = $Data->ToJSONString('{8F47273A-0B69-489E-AF36-F391AE5FBEC0}');
+        return @IPS_SendDataToParent($this->InstanceID, $JSONString);
+    }
+
+    // Command/response protocol - wait for response
+    private function WaitForResponse() {
+        $ResponseDataID = $this->GetIDForIdent('ResponseData');
+        for ($i = 0; $i < 300; $i++) {
+            if (GetValueString($ResponseDataID) === '') {
+                IPS_Sleep(5);
+            } else {
+                if ($this->lock('ResponseData')) {
+                    $ret = GetValueString($ResponseDataID);
+                    SetValueString($ResponseDataID, '');
+                    $this->unlock('ResponseData');
+                    $JSON = json_decode($ret);
+                    $Response = new ArygonResponseASCII();
+                    $Response->GetDataFromJSONObject($JSON);
+                    return $Response;
+                }
+            }
+        }
+        if ($this->lock('ResponseData')) {
+            SetValueString($ResponseDataID, '');
+            $this->unlock('ResponseData');
+        }
+
+        return false;
+    }
+
+    // Semaphore helpers
+
+    private function lock($ident) {
+        for ($i = 0; $i < 100; $i++) {
+            if (IPS_SemaphoreEnter("ADRA_" . (string) $this->InstanceID . (string) $ident, 1)) {
+                return true;
+            } else {
+                IPS_Sleep(mt_rand(1, 5));
+            }
+        }
+        return false;
+    }
+
+    private function unlock($ident) {
+        IPS_SemaphoreLeave("ADRA_" . (string) $this->InstanceID . (string) $ident);
+    }
+
+}
+
+?>
---
+++ ArygonSplitter/module.php
@@ -0,0 +1,176 @@
+<?
+
+require_once(__DIR__ . "/../ArygonData.php");
+
+class ArygonSplitter extends IPSModule {
+
+    // IPS module functions
+
+    public function Create() {
+        parent::Create();
+        $this->RequireParent("{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}");
+    }
+
+    public function ApplyChanges() {
+        parent::ApplyChanges();
+        $this->RegisterVariableString("BufferIn", "BufferIn", "", -1);
+        $this->RegisterVariableString("CommandOut", "CommandOut", "", -2);
+        IPS_SetHidden($this->GetIDForIdent('CommandOut'), true);
+        IPS_SetHidden($this->GetIDForIdent('BufferIn'), true);
+    }
+
+    private function CheckParents() {
+        $result = $this->HasActiveParent();
+        if ($result) {
+            $instance = IPS_GetInstance($this->InstanceID);
+            $parentGUID = IPS_GetInstance($instance['ConnectionID'])['ModuleInfo']['ModuleID'];
+            if ($parentGUID != '{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}') {
+                IPS_LogMessage('Arygon Splitter', 'IO-Parent not supported.');
+                $result = false;
+            }
+        }
+        return $result;
+    }
+
+    protected function HasActiveParent() {         
+        $instance = IPS_GetInstance($this->InstanceID);
+        if ($instance['ConnectionID'] > 0) {
+            $parent = IPS_GetInstance($instance['ConnectionID']);
+            if ($parent['InstanceStatus'] == 102) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected function SetStatus($InstanceStatus) {
+        if ($InstanceStatus <> IPS_GetInstance($this->InstanceID)['InstanceStatus']) {
+            parent::SetStatus($InstanceStatus);
+        }
+    }
+
+    // IPS raw data iterface for child (device) to parent (serial interface) forwarding
+    public function ForwardData($JSONString) {
+        $Data = json_decode($JSONString);
+        if ($Data->DataID <> "{62096A8D-6F10-4E1D-A51F-0EDFD09DCF44}") {
+            return false;
+        }
+        $Command = new ArygonCommandASCII();
+        $Command->GetDataFromJSONObject($Data);
+        try {
+            $this->ForwardDataFromChild($Command);
+            
+        } catch (Exception $ex) {
+            trigger_error($ex->getMessage(), $ex->getCode());
+            return false;
+        }
+        return true;
+    }
+
+    // Forward command from child (device) to parent (serial interface)
+    private function ForwardCommandFromChild(ArygonCommandASCII $Command) {
+        if (!$this->CheckParents()) {
+            throw new Exception("Instance has no active Parent.", E_USER_NOTICE);
+        }
+
+        $Raw = $Command->GetCommand();
+        IPS_LogMessage('ArygonSplitter', 'Command: ' . $Raw);
+
+        if (!$this->lock("ToParent")) {
+            throw new Exception("Can not send to Parent", E_USER_NOTICE);
+        }
+
+        try {
+            IPS_SendDataToParent($this->InstanceID, json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => utf8_encode($Raw))));
+        } catch (Exception $exc) {
+            $this->unlock("ToParent");
+            throw new Exception($exc);
+        }
+
+        $this->unlock("ToParent");
+        return true;
+    }
+
+    // IPS raw data iterface for parent (serial interface) to child (device) forwarding
+    public function ReceiveData($JSONString)
+    {
+        $data = json_decode($JSONString);
+        
+        $this->CheckParents();
+        
+        if ($this->Mode === false) {
+            trigger_error("Wrong IO-Parent", E_USER_WARNING);
+            return false;
+        }
+
+        $bufferID = $this->GetIDForIdent("BufferIn");
+        
+        if (!$this->lock("ReceiveLock")) {
+            trigger_error("ReceiveBuffer is locked", E_USER_NOTICE);
+            return false;
+        }
+
+        $head = GetValueString($bufferID);
+        SetValueString($bufferID, '');
+        $stream = $head . utf8_decode($data->Buffer);
+
+        $minTail = 8;
+        $start = strpos($stream, 'FF');
+        if ($start === false) {
+            IPS_LogMessage('Arygon Splitter', 'Response Packet without FF');
+            $stream = '';
+        } elseif ($start > 0) {
+            IPS_LogMessage('Arygon Splitter', 'Response Packet did not start with FF');
+            $stream = substr($stream, $start);
+        }
+        $end = strpos($stream, '\r\n');
+        if ($end === false) {
+            SetValueString($bufferID, $stream);
+            $this->unlock("ReceiveLock");
+            return;
+        } else {
+            $stream = substr($stream, 0, $end);
+        }
+
+        if(strlen($stream) < $minTail)) {
+            IPS_LogMessage('Arygon Splitter', 'Response Packet too short');
+            $this->unlock("ReceiveLock");
+            return;
+        }
+
+        $this->unlock("ReceiveLock");
+
+        $Response = new ArygonResponseASCII();
+        $Response->SetResponse($stream);
+        $this->SendResponseToChild($Response);
+        IPS_LogMessage('ArygonSplitter', 'Response: ' . $stream);
+
+        return true;
+    }
+
+    // Forward response from parent (serial interface) to child (device)
+    private function SendResponseToChild(ArygonResponseASCII $Response) {
+        $Data = $Response->ToJSONString('{43E4B48E-2345-4A9A-B506-3E8E7A964757}');
+        IPS_SendDataToChildren($this->InstanceID, $Data);
+    }
+
+    // Semaphore helpers
+
+    private function lock($ident) {
+        for ($i = 0; $i < 100; $i++) {
+            if (IPS_SemaphoreEnter("ADRAS_" . (string) $this->InstanceID . (string) $ident, 1)) {
+                return true;
+            } else {
+                IPS_Sleep(mt_rand(1, 5));
+            }
+        }
+        return false;
+    }
+
+    private function unlock($ident) {
+        IPS_SemaphoreLeave("ADRAS_" . (string) $this->InstanceID . (string) $ident);
+    }
+
+}
+
+?>
